c109d9ffb37ba0869f18a5f9dc8a9521ddb96996
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-04-05 20:46:49 +0300

    Initial Commit

---
+++ Stylesheet/StylesheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylesheetApp.swift
+//  Stylesheet
+//
+//  Created by Artem Kutasevich on 5.04.22.
+//
+
+import SwiftUI
+
+@main
+struct StylesheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
